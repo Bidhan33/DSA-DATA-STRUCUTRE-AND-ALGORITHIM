42bd96ebb3f74add8fdcbcb2292383c88aaf488d
Bidhan Adhikari <142262907+Bidhan33@users.noreply.github.com>
2024-11-13 11:33:48 +0200

    Update graph_class.js

--- graph_class.js
+++ graph_class.js
@@ -21,3 +21,32 @@ this.pathTo = pathTo;
 this.topSortHelper = topSortHelper; 
 this.topSort = topSort; 
 }
+function topSort() { 
+var stack = []; 
+var visited = []; 
+for (var i = 0; i < this.vertices; i++) { 
+visited[i] = false; 
+} 
+for (var i = 0; i < this.vertices; i++) { 
+if (visited[i] == false) { 
+this.topSortHelper(i, visited, stack); 
+} 
+} 
+for (var i = 0; i < stack.length; i++) { 
+if (stack[i] != undefined && stack[i] != false) { 
+print(this.vertexList[stack[i]]); 
+} 
+} 
+} 
+function topSortHelper(v, visited, stack) { 
+visited[v] = true; 
+for each (var w in this.adj[v]) { 
+if (!visited[w]) { 
+this.topSortHelper(visited[w], visited, stack); 
+} 
+} 
+stack.push(v); 
+} 
+function addEdge(v,w) { 
+this.adj[v].push(w); 
+this.adj[w].push(v);
