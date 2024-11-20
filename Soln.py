894c42b1dcc9ea6a54b309020bd2c78b95590902
Bidhan Adhikari <142262907+Bidhan33@users.noreply.github.com>
2024-11-20 22:21:04 +0200

    Update Soln.py

--- Soln.py
+++ Soln.py
@@ -17,3 +17,6 @@ arr = [11, 13, 21, 3]
 printNGE(arr)
 
 # Soln no 2 py   : few questions regarding this 
+# What value is printed if no greater element exists for an array element?
+How does the inner loop (for j in range(i+1, len(arr), 1)) contribute to finding the next greater element?
+What is the role of the break statement in the code?
